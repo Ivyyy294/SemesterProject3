f2f1f16d7647ac4bd32a6f8e8ad2d3d3155b7d23
Ivyyy294 <48586445+Ivyyy294@users.noreply.github.com>
2023-12-16 11:03:30 +0100

    :hammer: CHG ScannableObject
    Disable after targetScanTime is reached

--- Assets/01_Scripts/Scanner/ScannableObject.cs
+++ Assets/01_Scripts/Scanner/ScannableObject.cs
@@ -5,11 +5,41 @@ using Ivyyy.Network;
 
 public class ScannableObject : NetworkBehaviour
 {
-	int scannerCount;
+	[Min (0.1f)]
+	[SerializeField] float targetScanTime;
+	int scannerCount = 0;
+	bool active = true;
+	float scanTimer = 0f;
 
 	protected override void SetPackageData()
 	{
+		networkPackage.AddValue (new NetworkPackageValue (active));
+	}
 
+	private void Start()
+	{
+		if (NetworkManager.Me)
+			Owner = NetworkManager.Me.Host;
+		else
+			Owner = true;
+	}
+
+	private void Update()
+	{
+		if (Owner)
+		{
+			if (scannerCount > 0)
+				scanTimer += Time.deltaTime * scannerCount;
+			//Reset scanTimer if no player has lock
+			else
+				scanTimer = 0f;
+			
+			active = scanTimer < targetScanTime;
+		}
+		else if (networkPackage.Count > 0)
+			active = networkPackage.Value(0).GetBool();
+
+		gameObject.SetActive (active);
 	}
 
 	private void OnTriggerEnter(Collider other)
