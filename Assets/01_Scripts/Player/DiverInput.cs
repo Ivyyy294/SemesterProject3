11ad6d80c2849076d800746f191e61837815f377
Ivyyy294 <48586445+Ivyyy294@users.noreply.github.com>
2024-01-04 03:27:57 +0100

    :hammer: Opt DiverInput

--- Assets/01_Scripts/Player/DiverInput.cs
+++ Assets/01_Scripts/Player/DiverInput.cs
@@ -22,6 +22,7 @@ public class DiverInput : NetworkBehaviour
 	KeyCode forwardKey = KeyCode.Space;
     
 	Rigidbody _rigidbody;
+	int packageNr = 0;
 
 	protected override void SetPackageData()
 	{
@@ -32,6 +33,10 @@ public class DiverInput : NetworkBehaviour
 		networkPackage.AddValue (new NetworkPackageValue (transform.position));	//4
 		networkPackage.AddValue (new NetworkPackageValue (transform.rotation));	//5
 		networkPackage.AddValue (new NetworkPackageValue (forwardPressed));		//6
+		networkPackage.AddValue (new NetworkPackageValue (packageNr));			//7
+
+		if (Owner)
+			packageNr++;
 	}
 
 	void Start()
@@ -42,58 +47,47 @@ public class DiverInput : NetworkBehaviour
 
 	void Update()
     {
-		UpdateForwardInput();
-		UpdatePitch();
-		UpdateYaw();
+		if (!Owner && networkPackage.Available)
+		{
+			int newNr = networkPackage.Value (7).GetInt32();
 
-		if (!Owner)
-			UpdatePositionData();
+			if (newNr > packageNr)
+			{
+				_pitch = networkPackage.Value(1).GetFloat();
+				_pitchSway = networkPackage.Value(2).GetFloat();
+				_yaw = networkPackage.Value(2).GetFloat();
+				_yawSway = networkPackage.Value(3).GetFloat();
+				transform.position = networkPackage.Value(4).GetVector3();
+				transform.rotation = networkPackage.Value(5).GetQuaternion();
+				forwardPressed = networkPackage.Value(6).GetBool();
+				packageNr = newNr;
+			}
+		}
+
+		if (Owner)
+		{
+			UpdateForwardInput();
+			UpdatePitch();
+			UpdateYaw();
+		}
     }
 
     void UpdateForwardInput()
     {
-		if (Owner)
-			forwardPressed = Input.GetKey(forwardKey);
-		else if (networkPackage.Available)
-			forwardPressed = networkPackage.Value(6).GetBool();
+		forwardPressed = Input.GetKey(forwardKey);
     }
 
     void UpdatePitch()
     {
-		if (Owner)
-		{
-			_pitch = Input.GetAxisRaw("Vertical");
-			float diff = Mathf.Sign(_pitch - _pitchSway);
-			_pitchSway = Mathf.Clamp(_pitchSway + diff * Time.deltaTime, -1, 1);
-		}
-		else if (networkPackage.Available)
-		{
-			_pitch = networkPackage.Value(1).GetFloat();
-			_pitchSway = networkPackage.Value(2).GetFloat();
-		}
+		_pitch = Input.GetAxisRaw("Vertical");
+		float diff = Mathf.Sign(_pitch - _pitchSway);
+		_pitchSway = Mathf.Clamp(_pitchSway + diff * Time.deltaTime, -1, 1);
     }
 
     void UpdateYaw()
     {
-		if (Owner)
-		{
-			_yaw = Input.GetAxisRaw("Horizontal");
-			float diff = Mathf.Sign(_yaw - _yawSway);
-			_yawSway = Mathf.Clamp(_yawSway + diff * Time.deltaTime, -1, 1);
-		}
-		else if (networkPackage.Available)
-		{
-			_yaw = networkPackage.Value(2).GetFloat();
-			_yawSway = networkPackage.Value(3).GetFloat();
-		}
+		_yaw = Input.GetAxisRaw("Horizontal");
+		float diff = Mathf.Sign(_yaw - _yawSway);
+		_yawSway = Mathf.Clamp(_yawSway + diff * Time.deltaTime, -1, 1);
     }
-
-	void UpdatePositionData()
-	{
-		if (networkPackage.Available)
-		{
-			transform.position = networkPackage.Value(4).GetVector3();
-			transform.rotation = networkPackage.Value(5).GetQuaternion();
-		}
-	}
 }
