48073e9af42fb004420fefb97e3574aff2505834
Ivyyy294 <48586445+Ivyyy294@users.noreply.github.com>
2024-01-05 15:31:00 +0100

    :tada: New PlayerTeamIndex

---
+++ Assets/01_Scripts/Player/PlayerTeam.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeam : MonoBehaviour
+{
+	[SerializeField] int teamIndex = 0;
+	public int TeamIndex => teamIndex;
+}
