04f3153d7c24ca218f91b530bfdda37b953a6492
Ivyyy294 <48586445+Ivyyy294@users.noreply.github.com>
2024-01-05 14:01:04 +0100

    :sparkles: NEW PlayerMovementProfile

--- Assets/01_Scripts/Player/PlayerMovementProfile.cs
+++ Assets/01_Scripts/Player/PlayerMovementProfile.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementProfil : MonoBehaviour
+[CreateAssetMenu (fileName = "NewPlayerMovementProfil", menuName = "PlayerMovementProfil")]
+public class PlayerMovementProfil : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[Min(0f)]
+	public float maxSpeed = 1f;
+	[Min(0f)]
+	public float turnSpeedDegrees = 10f;
+	public AnimationCurve accelerationCurve;
+	public AnimationCurve deaccelerationCurve;
 }
