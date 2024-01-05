54fe1545ffa536f1511959429245e31cca73c617
Kolupsy <kolupsy@gmail.com>
2024-01-05 15:41:43 +0100

    ✨ADD temp team coloring

---
+++ Assets/01_Scripts/Player/TeamColor.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerTeam))]
+public class TeamColor : MonoBehaviour
+{
+    private MaterialPropertyBlock _mpb;
+    public MaterialPropertyBlock Mpb
+    { get
+        {
+            if (_mpb is null) _mpb = new MaterialPropertyBlock();
+            return _mpb;
+        }
+    }
+    
+    [SerializeField] private Renderer renderer;
+    [SerializeField] private List<Color> teamColors;
+
+    private PlayerTeam _playerTeam;
+
+    private void OnEnable()
+    {
+        _playerTeam = GetComponent<PlayerTeam>();
+    }
+
+    public void SetTeam(int teamIndex)
+    {
+        Mpb.SetColor("_BaseColor", teamColors[teamIndex]);
+        renderer.SetPropertyBlock(Mpb);
+    }
+
+    void Update()
+    {
+        SetTeam(_playerTeam.TeamIndex);
+    }
+}
