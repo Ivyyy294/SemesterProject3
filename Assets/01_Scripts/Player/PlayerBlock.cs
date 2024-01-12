ffb101c1358ccc86cc580cd59a4187721cd5a77a
Ivyyy294 <48586445+Ivyyy294@users.noreply.github.com>
2024-01-12 10:46:11 +0100

    :hammer: PlayerBlock
    Prevent block when oxygen is empty

--- Assets/01_Scripts/Player/PlayerBlock.cs
+++ Assets/01_Scripts/Player/PlayerBlock.cs
@@ -7,16 +7,21 @@ public class PlayerBlock : MonoBehaviour
 	[SerializeField] GameObject playerBlockCollider;
 
 	PlayerInput playerInput;
+	PlayerBallStatus playerBallStatus;
+	PlayerOxygen playerOxygen;
     // Start is called before the first frame update
     void Start()
     {
         playerInput = transform.parent.GetComponentInChildren<PlayerInput>();
+		playerBallStatus = transform.parent.GetComponentInChildren<PlayerBallStatus>();
+		playerOxygen = transform.parent.GetComponentInChildren <PlayerOxygen>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		bool block = playerInput.BlockPressed;
+		//Allow block when player dont has ball and oxygen is not empty
+		bool block = playerInput.BlockPressed && !playerBallStatus.HasBall() && !playerOxygen.OxygenEmpty;
 
 		if (playerBlockCollider.activeInHierarchy != block)
 			playerBlockCollider.SetActive (block);
