0e7c4887a36810488889e4d27c77b13cf7a5b35b
Ivyyy294 <48586445+Ivyyy294@users.noreply.github.com>
2024-01-18 10:17:30 +0100

    :ambulance:

--- Assets/01_Scripts/OxygenSystem/OxygenMeterUi.cs
+++ Assets/01_Scripts/OxygenSystem/OxygenMeterUi.cs
@@ -6,14 +6,12 @@ using TMPro;
 public class OxygenMeterUi : MonoBehaviour
 {
 	[SerializeField] TextMeshProUGUI oxygenLabel;
-	[SerializeField] PlayerManager playerManager;
 	PlayerOxygen playerOxygen;
 
     // Start is called before the first frame update
     void Start()
     {
-		if (playerManager)
-			playerOxygen = PlayerManager.LocalPlayer.GetComponentInChildren<PlayerOxygen>();
+		playerOxygen = PlayerManager.LocalPlayer.GetComponentInChildren<PlayerOxygen>();
     }
 
     // Update is called once per frame
