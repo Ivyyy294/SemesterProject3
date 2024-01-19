4e5c3d6b531ace2abeb376f3678d5c0feb17d872
Kolupsy <kolupsy@gmail.com>
2024-01-19 02:54:04 +0100

    ✨NEW OxygenAnimation script

---
+++ Assets/01_Scripts/Animation/DiverOxygenAnimation.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiverOxygenAnimation : MonoBehaviour
+{
+    [SerializeField] private DiverVisuals diverVisuals;
+    [SerializeField] private PlayerOxygen playerOxygen;
+    [SerializeField] private TeamColor teamColor;
+
+    private float _oxygen;
+    private Gauge _oxygenGainGauge = new(20, 1.5f);
+    private Color _oxygenFillColor = Color.white;
+
+    private void Start()
+    {
+        _oxygen = 0;
+    }
+
+    private void Update()
+    {
+        bool oxygenIncreased = false;
+        float newOxygen = GetOxygenState();
+        if (_oxygen < newOxygen)
+        {
+            oxygenIncreased = true;
+            Debug.Log("Oxygen Increased");
+        }
+        _oxygen = newOxygen;
+        _oxygenGainGauge.Update(oxygenIncreased);
+        Color resultColor = Color.Lerp(teamColor.Color, _oxygenFillColor, _oxygenGainGauge.FillAmount * 0.75f);
+        diverVisuals.emissiveColor = resultColor;
+        diverVisuals.oxygenLevel = _oxygen;
+    }
+
+    private float GetOxygenState()
+    {
+        return playerOxygen.CurrentOxygenPercent / 100;
+    }
+}
