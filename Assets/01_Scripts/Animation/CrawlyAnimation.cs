9fc71a41bed1db14aa79f76eada40b9903f57c81
Kolupsy <kolupsy@gmail.com>
2024-01-23 14:49:40 +0100

    🍱UPDATE Crawly Size + Dynamic Movement

--- Assets/01_Scripts/Animation/CrawlyAnimation.cs
+++ Assets/01_Scripts/Animation/CrawlyAnimation.cs
@@ -1,15 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using AnimUtils;
 using UnityEngine;
 
 public class CrawlyAnimation : MonoBehaviour
 {
     [Header("Local References")]
     [SerializeField] private Animator animator;
+    [SerializeField] private DiverVerletBehavior verletBehavior;
 
-    [Header("Parameters")] [SerializeField]
-    private float turnSpeedDegrees = 30f;
+    [Header("Parameters")] 
+    [SerializeField] private float turnSpeedDegrees = 30f;
+    [SerializeField] private float wiggleStrength = 2f;
+
+    [Header("Joints References")] 
+    [SerializeField] private List<Transform> lowerBodyChain;
 
     #region AnimatorParameterIDs
 
@@ -19,15 +25,31 @@ public class CrawlyAnimation : MonoBehaviour
 
     private VelocityTracker _velocityTracker;
     private Vector3 _movementVector;
+    
+    private Gauge _wiggleFactorGauge;
+    private bool _wiggleActive = true;
+    
+    private AngleTracker _inertiaTracker;
 
     private void OnEnable()
     {
         _velocityTracker = new VelocityTracker(transform.position, 8);
         _movementVector = transform.forward;
+        verletBehavior.ResetSimulation();
+        _wiggleFactorGauge = new Gauge(4, 4);
+        _wiggleFactorGauge.SetFillAmount(1);
     }
 
     private void Update()
     {
+        _inertiaTracker = new(
+            transform.position, 
+            verletBehavior.SmoothTarget, 
+            -transform.forward, 
+            transform.right,
+            transform.up);
+        
+        _wiggleFactorGauge.Update(_wiggleActive);
 
         float levelness = 1 - Mathf.Abs(Vector3.Dot(transform.forward, Vector3.up));
         float twist = Mathf.Abs(Vector3.Dot(transform.right, Vector3.up));
@@ -54,7 +76,28 @@ public class CrawlyAnimation : MonoBehaviour
     {
         _velocityTracker.FixedUpdate(transform.position);
     }
-    
+
+    private void LateUpdate()
+    {
+        if(_inertiaTracker != null) UpdateSimulatedLimbs();
+    }
+
+    private void UpdateSimulatedLimbs()
+    {
+        var pitchAngle = _inertiaTracker.Angle1;
+        var yawAngle = _inertiaTracker.Angle2;
+
+        var multiplier = wiggleStrength * _wiggleFactorGauge.FillAmount;
+        multiplier /= lowerBodyChain.Count;
+        
+        for (int i = 0; i < lowerBodyChain.Count; i++)
+        {
+            var t = lowerBodyChain[i];
+            t.Rotate(Vector3.right, pitchAngle * multiplier);
+            t.Rotate(Vector3.forward, yawAngle * multiplier);
+        }
+    }
+
     Vector3 GetIdealRightVector()
     {
         return Vector3.Cross(Vector3.up, transform.forward).normalized;
