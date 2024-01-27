eb4c2634cff8d68f304a75e6c23c9e19ceb19d7f
Ivyyy294 <48586445+Ivyyy294@users.noreply.github.com>
2024-01-27 13:17:43 +0100

    :tada: Audio trigger OxygenLevel

--- Assets/01_Scripts/OxygenSystem/OxygenMeterUi.cs
+++ Assets/01_Scripts/OxygenSystem/OxygenMeterUi.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Ivyyy.Utils;
 
 public class OxygenMeterUi : MonoBehaviour
 {
+	[Header ("Audio")]
+	[SerializeField] AudioAsset audio75;
+	[SerializeField] AudioAsset audio50;
+	[SerializeField] AudioAsset audio25;
+
+	[Header ("Lara Values")]
 	[SerializeField] TextMeshProUGUI oxygenLabel;
 	PlayerOxygen playerOxygen;
+	
+	BitSet audioMemory = new BitSet (1);	
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +28,37 @@ public class OxygenMeterUi : MonoBehaviour
     {
         if (oxygenLabel && playerOxygen)
 			oxygenLabel.text = ((int)playerOxygen.CurrentOxygenPercent).ToString();
+
+		UpdateAudioMemory();
+		PlayAudio();
     }
+
+	void UpdateAudioMemory()
+	{
+		if (playerOxygen.CurrentOxygenPercent > 75f && audioMemory.Check(0))
+			audioMemory.SetBit (0, false);
+		if (playerOxygen.CurrentOxygenPercent > 50f && audioMemory.Check(0))
+			audioMemory.SetBit (1, false);
+		if (playerOxygen.CurrentOxygenPercent > 25f && audioMemory.Check(0))
+			audioMemory.SetBit (2, false);
+	}
+
+	void PlayAudio()
+	{
+		if (playerOxygen.CurrentOxygenPercent <= 25f)
+			PlayAudio (audio25, 2);
+		else if (playerOxygen.CurrentOxygenPercent <= 50f)
+			PlayAudio (audio50, 1);
+		else if (playerOxygen.CurrentOxygenPercent <= 75f)
+			PlayAudio (audio75, 0);
+	}
+
+	void PlayAudio (AudioAsset audioAsset, int bitNr)
+	{
+		if (!audioMemory.Check (bitNr))
+		{
+			audioAsset.Play();
+			audioMemory.SetBit (bitNr, true);
+		}
+	}
 }
