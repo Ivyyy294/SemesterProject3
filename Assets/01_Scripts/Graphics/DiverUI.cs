e4b7aeefdc7dfc4decb981e0cdd87f24962bce70
Kolupsy <kolupsy@gmail.com>
2024-01-29 21:46:12 +0100

    ✨NEW InGameUIBackground

---
+++ Assets/01_Scripts/Graphics/DiverUI.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class DiverUI : MonoBehaviour
+{
+    [SerializeField] private TeamColorSettings teamColors;
+    [Range(0, 1)] public float oxygen = 0.5f;
+    [SerializeField] private Color oxygenColor = new Color(0.3f, 0.6f, 1);
+    
+    private Image _image;
+    private Gauge _oxygenFillGauge = new(30, 1);
+    private float _previousOxygen;
+    
+    public static DiverUI Me { get; private set; }
+
+    #region MaterialPropertyIDs
+
+    private readonly int ID_Slider = Shader.PropertyToID("_Slider");
+    private readonly int ID_Team1 = Shader.PropertyToID("_Team1");
+    private readonly int ID_Team2 = Shader.PropertyToID("_Team2");
+    private readonly int ID_OxygenColor = Shader.PropertyToID("_OxygenColor");
+
+    #endregion
+
+    private void Awake()
+    {
+        if (Me == null) Me = this;
+        else Destroy(this);
+    }
+
+    private void OnEnable()
+    {
+        _image = GetComponent<Image>();
+        _oxygenFillGauge.SetFillAmount(0);
+        _previousOxygen = oxygen;
+    }
+
+    void Update()
+    {
+        _oxygenFillGauge.Update(_previousOxygen < oxygen);
+        _previousOxygen = oxygen;
+        
+        _image.material.SetColor(ID_Team1, teamColors.GetTeamColor(0));
+        _image.material.SetColor(ID_Team2, teamColors.GetTeamColor(1));
+        _image.material.SetFloat(ID_Slider, oxygen);
+        var finalColor = Color.Lerp(oxygenColor, Color.white, _oxygenFillGauge.FillAmount * 0.5f);
+        _image.material.SetColor(ID_OxygenColor, finalColor);
+
+    }
+}
