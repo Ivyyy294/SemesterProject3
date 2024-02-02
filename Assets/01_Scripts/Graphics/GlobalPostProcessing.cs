a8d91a817515eb65fa96c22bade84b02f809101b
Kolupsy <kolupsy@gmail.com>
2024-02-03 00:48:47 +0100

    🔨GlobalPostProcessing

--- Assets/01_Scripts/Graphics/GlobalPostProcessing.cs
+++ Assets/01_Scripts/Graphics/GlobalPostProcessing.cs
@@ -25,11 +25,17 @@ public class GlobalPostProcessing : MonoBehaviour
             return _me;
         }
     }
-    
-    private FullScreenPassRendererFeature _feature;
-    private Material _originalMaterial;
-    private Material _tempMaterial;
-    
+
+    #region FeaturesAndMaterials
+
+    private FullScreenPassRendererFeature _wobbleFeature;
+    private RuntimeMaterial _wobbleMaterial;
+
+    private FullScreenPassRendererFeature _transitionFeature;
+    private RuntimeMaterial _transitionMaterial;
+
+    #endregion
+
     #region MaterialParameters
     
     private readonly int ID_NoiseTile = Shader.PropertyToID("_NoiseTile");
@@ -37,8 +43,11 @@ public class GlobalPostProcessing : MonoBehaviour
     private readonly int ID_Speed = Shader.PropertyToID("_Speed");
     private readonly int ID_MaskCenter = Shader.PropertyToID("_MaskCenter");
     private readonly int ID_SpeedLines = Shader.PropertyToID("_SpeedLines");
+    private readonly int ID_Transition = Shader.PropertyToID("_Transition");
     
     #endregion
+
+    private Timer _transition = new(1);
 
     private void Awake()
     {
@@ -60,27 +69,44 @@ public class GlobalPostProcessing : MonoBehaviour
         var property = typeof(ScriptableRenderer).GetProperty("rendererFeatures", BindingFlags.NonPublic | BindingFlags.Instance);
         List<ScriptableRendererFeature> rendererFeatures = property.GetValue(universalRenderer) as List<ScriptableRendererFeature>;
         
+        
+        
         var feature = rendererFeatures?.OfType<FullScreenPassRendererFeature>()
             .FirstOrDefault(x => x.name.Contains("Wobble"));
-        if (feature is null) return;
-        
-        _feature = feature;
-        _originalMaterial = _feature.passMaterial;
-        _tempMaterial = new Material(_originalMaterial);
-        _tempMaterial.hideFlags = HideFlags.HideAndDontSave;
-        _feature.passMaterial = _tempMaterial;
+        if (feature is not null)
+        {
+            _wobbleFeature = feature;
+            _wobbleMaterial = new(_wobbleFeature.passMaterial);
+            _wobbleFeature.passMaterial = _wobbleMaterial.Mat;
+        }
+
+        feature = rendererFeatures?.OfType<FullScreenPassRendererFeature>().First(x => x.name.Contains("Transition"));
+        if (feature is not null)
+        {
+            _transitionFeature = feature;
+            _transitionMaterial = new(_transitionFeature.passMaterial);
+            _transitionFeature.passMaterial = _transitionMaterial.Mat;
+        }
+
+
     }
 
     private void OnDisable()
     {
-        if (_feature is null) return;
-        _feature.passMaterial = _originalMaterial;
-        Destroy(_tempMaterial);
+        if (_wobbleFeature is null) return;
+        _wobbleFeature.passMaterial = _wobbleMaterial.Release();
+        _transitionFeature.passMaterial = _transitionMaterial.Release();
     }
 
     private void Update()
     {
-        _tempMaterial.SetFloat(ID_SpeedLines, speedLines);
+        _transition.Update();
+        _wobbleMaterial.Mat.SetFloat(ID_SpeedLines, speedLines);
+    }
+
+    public void Transition()
+    {
+        _transition.Start();
     }
 
     public void ResetEffects()
